21f276483fdf20b8971b75beb187c10943ec0084
Kangjin Kim <testksquared@users.noreply.github.com>
2026-06-08 11:49:05 +0900

    Add Seoyoung Bae profile photo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const peopleContainer = document.querySelector("#people-content");
 const researchContainer = document.querySelector("#research-content");
 const galleryContainer = document.querySelector("#gallery-content");
 const projectsContainer = document.querySelector("#projects-content");
-const dataVersion = "20260608-pi-metadata";
+const dataVersion = "20260608-seoyoung";
 
 const defaultGroups = [
   { key: "principalInvestigator", title: "Principal Investigator" },
