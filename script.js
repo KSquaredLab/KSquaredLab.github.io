ab4826288f8414f9c95a7514d48b24379c500b1b
Kangjin Kim <testksquared@users.noreply.github.com>
2026-06-18 04:39:56 +0900

    Add Na Yoon Kim to people section

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const peopleContainer = document.querySelector("#people-content");
 const researchContainer = document.querySelector("#research-content");
 const galleryContainer = document.querySelector("#gallery-content");
 const projectsContainer = document.querySelector("#projects-content");
-const dataVersion = "20260611-jeju-gallery-order";
+const dataVersion = "20260618-na-yoon-kim";
 
 const defaultGroups = [
   { key: "principalInvestigator", title: "Principal Investigator" },
